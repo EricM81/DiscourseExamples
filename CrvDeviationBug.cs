f223b76b2cf57d86e0dab7e0d6df41ba8ab578fb
EricM <emarvets@gmail.com>
2023-06-04 09:00:14 -0400

    updated with @dale's code.

--- CrvDeviationBug.cs
+++ CrvDeviationBug.cs
@@ -18,52 +18,63 @@ namespace DiscourseExamples
 
         public override string EnglishName => "CrvDeviationBug";
 
-
         protected override Result RunCommand(RhinoDoc doc, RunMode mode)
         {
-            var crv0RO = doc.Objects.FindId(new Guid("f8d4bfbb-c324-42b9-b9e6-d362bf066726"));
-            var crv1RO = doc.Objects.FindId(new Guid("667a871b-362f-4542-abb1-fe5c99b233c0"));
-            if (crv0RO == null || crv1RO == null)
-            {
-                RhinoApp.WriteLine("Couldn't find the example crvs by their GUID.");
+            var go = new GetObject { GeometryFilter = ObjectType.Curve };
+            go.SetCommandPrompt("Select curves to test");
+            go.GetMultiple(2, 2);
+            if (go.CommandResult() != Result.Success)
+                return go.CommandResult();
+            
+            var curveA = go.Object(0).Curve();
+            var curveB = go.Object(1).Curve();
+            if (null == curveA || null == curveB)
                 return Result.Failure;
-            }
-           
-            if (crv0RO.Geometry is NurbsCurve crv0 && crv1RO.Geometry is NurbsCurve crv1)
-            {
-                var min = 0.0;
-                var minT0 = 0.0;
-                var minT1 = 0.0;
-                var max = 0.0;
-                var maxT0 = 0.0;
-                var maxT1 = 0.0;
-                if (Curve.GetDistancesBetweenCurves(crv0, crv1, 0.001,
-                        out max, out maxT0, out maxT1,
-                        out min, out minT0, out minT1))
-                {
-                    var d0 = crv0.Domain.T1 - crv0.Domain.T0;
-                    var d1 = crv1.Domain.T1 - crv0.Domain.T0;
-                    
-                    var t0 = crv0.Domain.NormalizedParameterAt(maxT0);
-                    var t1 = crv1.Domain.NormalizedParameterAt(maxT1);
-                    var pt0 = crv0.PointAtNormalizedLength(t0);
-                    var pt1 = crv1.PointAtNormalizedLength(t1);
-                    
-                    RhinoApp.WriteLine("Max: {0:N3}, MaxT0: {1:N3}, MaxT1: {2:N3}.", max, maxT0, maxT1);
-                    RhinoApp.WriteLine("Domain - Crv0: {0:N3}, Crv0: {1:N3}", d0, d1);
-                    RhinoApp.WriteLine("Normalized t - Crv0: {0:N3}, Crv1: {1:N3}", t0, t1);
 
-                    var line = new Line(pt0, pt1);
-                    doc.Objects.AddLine(line);
-                    RhinoApp.WriteLine("Distance from Pts at Normalized t's: {0:N3}", line.Length);
-                    doc.Views.Redraw();
-                    
-                }
+            if (Curve.GetDistancesBetweenCurves(curveA, curveB, 0.001,
+                    out var max, out var maxAt, out var maxBt,
+                    out var min, out _, out _))
+            {
+                // This is what I thought was bugged.  Converting it to the domain is not as simple as 
+                // using maxAt / (T1 - T0).  The knot vector gives the domain a non linear distribution.  
+                // This applies to both uniform and non-uniform crvs with the exception of single-spans. 
+                var d0 = curveA.Domain.T1 - curveA.Domain.T0;
+                var d1 = curveB.Domain.T1 - curveA.Domain.T0;
                 
-                return Result.Success;
+                var t0 = curveA.Domain.NormalizedParameterAt(maxAt);
+                var t1 = curveB.Domain.NormalizedParameterAt(maxBt);
+                var pt0 = curveA.PointAtNormalizedLength(t0);
+                var pt1 = curveB.PointAtNormalizedLength(t1);
+                
+                RhinoApp.WriteLine("Max: {0:N3}, MaxT0: {1:N3}, MaxT1: {2:N3}.", max, maxAt, maxBt);
+                RhinoApp.WriteLine("Domain - Crv0: {0:N3}, Crv0: {1:N3}", d0, d1);
+                RhinoApp.WriteLine("Normalized t - Crv0: {0:N3}, Crv1: {1:N3}", t0, t1);
+        
+                var lineEric = new Line(pt0, pt1);
+                doc.Objects.AddLine(lineEric);
+                RhinoApp.WriteLine("Distance from Pts at Normalized t's: {0:N3}", lineEric.Length);
+                
+                // From @dale
+                // using PointAt finds the correct spot in the domain.
+                var line = new Line
+                {
+                    From = curveA.PointAt(maxAt),
+                    To = curveB.PointAt(maxBt)
+                };
+                
+                doc.Objects.AddLine(line);
+                doc.Objects.AddPoint(line.From);
+                doc.Objects.AddPoint(line.To);
+                
+                RhinoApp.WriteLine("Minimum deviation = {0}", min);
+                RhinoApp.WriteLine("Maximum deviation = {0}", max);
+                
+                doc.Views.Redraw();
+                
             }
-            RhinoApp.WriteLine("Couldn't find the example crvs by their GUID.");
-            return Result.Failure;
+            
+            return Result.Success;
+       
         }
 
        
